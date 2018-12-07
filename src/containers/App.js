c289e191d91ebcf0eeda3c704436b00cfff6be5f
Ruslan Farutdinov <ruslanfarutdinov@gmail.com>
2018-12-07 09:52:08 -0500

    Setup folder structure and delete unneeded files

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const MainContainer = styled.div`
+  color: red;
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <MainContainer>
+        TEST
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
